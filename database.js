3fe68e9f5612ebddba89755ae33195646c1ae9eb
jon <jonathan.lazarini@wipro.com>
2017-02-10 18:12:18 +0000

    add database script - WORKING

--- database.js
+++ database.js
@@ -1,3 +1,45 @@
 /**
  * Created by jonlazarini on 10/02/17.
  */
+var MongoClient = require('mongodb').MongoClient;
+var config = require('./config');
+
+var dbUrl = config.databaseUrl;
+
+// function to interact with the DB on our behalf where the db gets injected
+var shoveData = function(something, timestamp, db, cb) {
+    // creates a collection visits to store our objects
+    db.collection('visits')
+        .insertOne({
+            something: something,
+            timestamp: timestamp
+            // callback when db is accessed to query it
+        }, function(err, result) {
+            console.log('result: ', result)
+            if (err) return cb(err)
+            // visits collection has been mutated
+            db.collection('visits')
+                .count()
+                // returns a promise with the visits from the callback when DB has been queried
+                .then(function(visits) {
+                    console.log('visits: ', visits)
+                    // gives back the result without err (null)
+                    cb(null, visits)
+                })
+        })
+
+};
+
+
+module.exports = {
+    logReq: function(something, timestamp, cb) {
+        // connects to mongodb and get access to the database in the callback
+        MongoClient.connect(dbUrl, function(err, db) {
+            // accesses our db post the data inside
+            shoveData(something, timestamp, db, function(err, result) {
+                db.close();
+                cb(err, result)
+            })
+        });
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+var database = require('../database');
+
+router.get('/', function(req, res, next) {
+
+  var something = 'some data'
+  var timestamp = new Date().toISOString()
+
+  // pass in data to be given in the callback
+  database.logReq(something, timestamp, function(err, result) {
+    // returns the template with the data from our database
+    res.send('<h1>'+something+'<h1><br><h2>'+timestamp+'<h2>')
+  })
+
 });
 
 module.exports = router;
